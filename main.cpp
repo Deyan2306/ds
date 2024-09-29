42f9cc36d9f0829b013c55015ab3869c64cb4f34
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-09-29 17:35:38 +0300

    some other minor changes over the overall look

--- main.cpp
+++ main.cpp
@@ -12,10 +12,25 @@
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
+/**
+ * `-r` for recursive call
+ * `-a` for all
+ * `-l` to list all 
+ * 
+ * ToDo's
+ * 1. sorting?
+ * 2. human readable formats
+ * 3. refactoring...
+ * 4. colors! 
+ *
+*/
+
 void print_file_info(const fs::directory_entry & entry) {
     struct stat fileStat;
+
     if (stat(entry.path().c_str(), &fileStat) == 0) {
-        // Права
+        
+        std::cout << "["; // == Opening bracket for the line 
         std::cout << ((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
         std::cout << ((fileStat.st_mode & S_IRUSR) ? "r" : "-");
         std::cout << ((fileStat.st_mode & S_IWUSR) ? "w" : "-");
@@ -26,26 +41,34 @@ void print_file_info(const fs::directory_entry & entry) {
         std::cout << ((fileStat.st_mode & S_IROTH) ? "r" : "-");
         std::cout << ((fileStat.st_mode & S_IWOTH) ? "w" : "-");
         std::cout << ((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+        std::cout << "]"; // == Closing bracket for the line
 
-        // Брой линкове
+        // Number of links
         std::cout << " " << fileStat.st_nlink;
 
-        // Собственик и група
-        struct passwd *pw = getpwuid(fileStat.st_uid);
-        struct group  *gr = getgrgid(fileStat.st_gid);
+        // Group and owner
+        struct passwd * pw = getpwuid(fileStat.st_uid);
+        struct group  * gr = getgrgid(fileStat.st_gid);
+
         std::cout << " " << (pw ? pw->pw_name : "unknown");
         std::cout << " " << (gr ? gr->gr_name : "unknown");
 
-        // Размер
+        // Size
         std::cout << " " << fileStat.st_size;
 
-        // Време на последна модификация
+        // Last modification time
         std::cout << " " << std::ctime(&fileStat.st_mtime);
 
-        // Име на файла
+        // File name
         std::cout << " " << entry.path().filename().string() << std::endl;
     }
 
+}
+
+void list_directory_recursive(const fs::path & folder_path) {
+    for (auto& entry : fs::recursive_directory_iterator(folder_path)) {
+        std::cout << entry.path().filename().string() << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -55,7 +78,8 @@ int main(int argc, char* argv[]) {
         ("help,h", "produce help message")
         ("path,p", po::value<std::string>()->default_value("."), "path to directory")
         ("all,a", "show the hidden files too")
-        ("long,l", "pring long information about the files");
+        ("long,l", "pring long information about the files")
+        ("recursive,r", "recursive iteration over the folder");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -76,20 +100,29 @@ int main(int argc, char* argv[]) {
 
     bool show_all = vm.count("all");
     bool long_info = vm.count("long");
+    bool recursive = vm.count("recursive");
 
     // Iterate the directory
-    for (auto & entry : fs::directory_iterator(folder_path)) {
-        std::string filename_path = entry.path().filename().string();
-        if (!show_all && filename_path[0] == '.') {
-            continue;
-        }
+    if (recursive) {
+        list_directory_recursive(folder_path);
+    } else {
+        for (auto & entry : fs::directory_iterator(folder_path)) {
+           std::string filename_path = entry.path().filename().string();
+            if (!show_all && filename_path[0] == '.') {
+                continue;
+            }   
 
-        if (long_info) {
-            print_file_info(entry);
-        } else {
-            std::cout << filename_path << std::endl;
+            if (long_info) {
+                print_file_info(entry);
+            } else {
+                if (entry.is_directory()) {
+
+                }
+
+                std::cout << " ⇢ " << filename_path << (entry.is_directory() ? "/ 🗁 " : "") << std::endl;
+            }   
         }   
     }
-
+    
     return 0;
 }
